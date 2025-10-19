68086773074748c2d8a661293e2f8412e5ac9b44
Robin Chan <robinasaurousrex@gmail.com>
2025-10-19 13:39:30 -0700

    complete mobile nav styles

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector("#mobileNav");
+const mobileMenu = document.querySelector(".mobile-menu");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
-const mobileNav = document.querySelector("#mobileNav");
 const bar1Color = document.querySelector(".bar1");
 const bar3Color = document.querySelector(".bar3");
 
+
+let toggle = true;
+
 hamburger.addEventListener("click", () => {
+    toggle = !toggle;
+
+    if (toggle) {
+        mobileNav.style.display = "none";
+        hamburger.style.opacity = 1;
+    } else {
+        mobileNav.style.display = "flex";
+        hamburger.style.opacity = 0.5;
+    }
+
+    if (mobileNav) {
+        mobileNav.addEventListener('click', () => {
+            mobileMenu.classList.toggle('active');
+        });
+    }
+
     bar1.classList.toggle("animateBar1");
     bar2.classList.toggle("animateBar2");
     bar3.classList.toggle("animateBar3");
-
-    mobileNav.classList.toggle("is-open");
-    const isExpanded = hamburger.classList.contains('is-open');
-    hamburger.setAttribute('aria-expanded', isExpanded);
-
     bar1Color.classList.toggle("changeColor");
     bar3Color.classList.toggle("changeColor");
 })
