3f35f7e76b3914f55cbf843c45fa5357feea7e5f
Robin Chan <robinasaurousrex@gmail.com>
2025-11-29 15:22:28 -0700

    fix issues in HTML markup and CSS stylesheet

--- index.js
+++ index.js
@@ -7,29 +7,28 @@ const bar3 = document.querySelector(".bar3");
 const bar1Color = document.querySelector(".bar1");
 const bar3Color = document.querySelector(".bar3");
 
-
 let toggle = true;
 
 hamburger.addEventListener("click", () => {
-    toggle = !toggle;
+  toggle = !toggle;
 
-    if (toggle) {
-        mobileNav.style.display = "none";
-        hamburger.style.opacity = 1;
-    } else {
-        mobileNav.style.display = "flex";
-        hamburger.style.opacity = 0.5;
-    }
+  if (toggle) {
+    mobileNav.style.display = "none";
+    hamburger.style.opacity = 1;
+  } else {
+    mobileNav.style.display = "flex";
+    hamburger.style.opacity = 0.5;
+  }
 
-    if (mobileNav) {
-        mobileNav.addEventListener('click', () => {
-            mobileMenu.classList.toggle('active');
-        });
-    }
+  if (mobileNav) {
+    mobileNav.addEventListener("click", () => {
+      mobileMenu.classList.toggle("active");
+    });
+  }
 
-    bar1.classList.toggle("animateBar1");
-    bar2.classList.toggle("animateBar2");
-    bar3.classList.toggle("animateBar3");
-    bar1Color.classList.toggle("changeColor");
-    bar3Color.classList.toggle("changeColor");
-})
+  bar1.classList.toggle("animateBar1");
+  bar2.classList.toggle("animateBar2");
+  bar3.classList.toggle("animateBar3");
+  bar1Color.classList.toggle("changeColor");
+  bar3Color.classList.toggle("changeColor");
+});
